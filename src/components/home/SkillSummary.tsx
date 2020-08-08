13cc9ef53b0084ac065d3ebb66469e286111c480
Boccochan <boccochan@gmail.com>
2020-08-08 11:34:48 +0900

    Add summary title

--- src/components/home/SkillSummary.tsx
+++ src/components/home/SkillSummary.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Radar,
   RadarChart,
   PolarGrid,
   PolarAngleAxis,
   PolarRadiusAxis,
+  LabelList,
 } from "recharts";
 import styled from "styled-components";
 
@@ -17,6 +18,39 @@ const Title = styled.div`
 const Container = styled.div`
   width: ${(props: { width: string }) => props.width};
 `;
+
+function customTick({
+  payload,
+  x,
+  y,
+  textAnchor,
+  stroke,
+  radius,
+}: {
+  payload: { value: string };
+  x: number;
+  y: number;
+  textAnchor: string;
+  stroke: string;
+  radius: string;
+}) {
+  return (
+    <g>
+      <text
+        radius={radius}
+        stroke={stroke}
+        x={x}
+        y={y}
+        text-anchor={textAnchor}
+        fontSize="12px"
+      >
+        <tspan x={x} dy="0em">
+          {payload.value}
+        </tspan>
+      </text>
+    </g>
+  );
+}
 
 export default function Skills() {
   const data = [
@@ -45,22 +79,46 @@ export default function Skills() {
       A: 30,
     },
   ];
+
+  const [ContainerWidth, setContainerWidth] = useState(0);
+  const [OuterRadius, setRadiusSize] = useState(100);
+
+  const resizeWindow = () => {
+    const innerWidth = window.innerWidth;
+    if (innerWidth > 1080) {
+      setRadiusSize(100);
+      setContainerWidth(innerWidth / 2);
+    } else if (320 < innerWidth && innerWidth <= 1080) {
+      setRadiusSize(100);
+      setContainerWidth(innerWidth);
+    } else {
+      setRadiusSize(60);
+      setContainerWidth(innerWidth);
+    }
+  };
+
+  useEffect(() => {
+    resizeWindow();
+    window.addEventListener("resize", resizeWindow);
+    return () => window.removeEventListener("resize", resizeWindow);
+  }, []);
+
   return (
-    <Container width="540px">
+    <Container width={`${ContainerWidth}px`}>
       <Title>Skill summary</Title>
       <RadarChart
         cx="50%"
         cy="50%"
-        outerRadius={100}
-        width={540}
+        outerRadius={OuterRadius}
+        width={ContainerWidth}
         height={300}
         data={data}
       >
         <PolarGrid />
-        <PolarAngleAxis dataKey="subject" />
+        <PolarAngleAxis dataKey="subject" tick={customTick} />
         <PolarRadiusAxis domain={[0, 100]} />
         <Radar
-          name="Mike"
+          name="Yasuhiro"
           dataKey="A"
           stroke="#8884d8"
           fill="#8884d8"
