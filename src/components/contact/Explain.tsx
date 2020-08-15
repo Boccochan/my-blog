d4e4b264fe2fdb070e8b8e8bb64e62e0bc6b4498
Boccochan <boccochan@gmail.com>
2020-08-15 14:06:05 +0900

    Refactor ballon

--- src/components/contact/Explain.tsx
+++ src/components/contact/Explain.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import { myMedia } from "@src/styles/custom-media";
 import { T3 } from "@src/styles/typography";
 import { theme } from "@src/styles/color";
 import PurpleRobotImg from "@static/home/purple-robot-min.png";
+import { Ballon } from "./Ballon";
 
 const Container = styled.div`
   position: relative;
@@ -20,58 +21,6 @@ const Container = styled.div`
 const StyledT3 = styled(T3)`
   padding: 4px;
   border-bottom: 1px solid ${theme.colors.graySuperLight};
-`;
-
-const Fadein = keyframes`
-  from {
-    opacity: 0;
-    transform: translateY(50px);
-  }
-  to {
-    opacity: 1;
-    transform: translateY(0);
-  }
-`;
-
-const Ballon = styled.div`
-  position: relative;
-  display: inline-block;
-  margin: 4em 0;
-  padding: 7px 10px;
-  min-width: 120px;
-  max-width: 100%;
-  color: #555;
-  font-size: 16px;
-  background: #fff;
-  border: solid 3px #555;
-  box-sizing: border-box;
-  font-weight: 300;
-  line-height: 22px;
-  font-size: 16px;
-  opacity: 0;
-  animation: ${Fadein} 1s ease forwards 0.2s;
-
-  &:before {
-    content: "";
-    position: absolute;
-    bottom: -24px;
-    left: 50%;
-    margin-left: -15px;
-    border: 12px solid transparent;
-    border-top: 12px solid #fff;
-    z-index: 2;
-  }
-
-  &:after {
-    content: "";
-    position: absolute;
-    bottom: -30px;
-    left: 50%;
-    margin-left: -17px;
-    border: 14px solid transparent;
-    border-top: 14px solid #555;
-    z-index: 1;
-  }
 `;
 
 const Description = styled.div`
@@ -93,7 +42,7 @@ export default () => {
     <Container>
       <StyledT3>Contact Form</StyledT3>
       <Description>
-        <Ballon>
+        <Ballon time="0.2s" margin="4em 0">
           IF YOU HAVE QUESTIONS, PLEASE LET ME KNOW. YOU DON'T NEED TO HESITATE
           TO SEND ME MESSAGES.
         </Ballon>
--- src/components/contact/Thanks.tsx
+++ src/components/contact/Thanks.tsx
@@ -4,13 +4,34 @@ import { theme } from "@src/styles/color";
 import { T2 } from "@src/styles/typography";
 import { myMedia } from "@src/styles/custom-media";
 import GrayDogRobot from "@static/home/gray-dog-robot-min.png";
+import { Ballon } from "./Ballon";
+
+const Close = keyframes`
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+    z-index: 0;
+  }
+`;
+
+const Wrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  animation: ${Close} 1s ease forwards 3.6s;
+  z-index: 10000;
+`;
 
 const Cover = styled.div`
   position: absolute;
   top: 0;
   left: 0;
   background-color: ${theme.colors.grayDark};
-  z-index: 10000;
+  z-index: 10001;
   width: 100%;
   height: 100%;
   opacity: 0.8;
@@ -36,7 +57,7 @@ const Dialog = styled.div`
   margin: auto;
   z-index: 10010;
   width: 100%;
-  heigth: 200px;
+  height: 80px;
   background-color: rgba(255, 255, 255, 1);
   border: 1px solid rgba(255, 255, 255, 1);
   border-radius: 5px;
@@ -76,57 +97,6 @@ const Img = styled.img`
   background-color: transparent;
 `;
 
-const Fadein = keyframes`
-  from {
-    opacity: 0;
-    transform: translateY(50px);
-  }
-  to {
-    opacity: 1;
-    transform: translateY(0);
-  }
-`;
-
-const Ballon = styled.div`
-  position: relative;
-  margin: 2em 0;
-  padding: 7px 10px;
-  min-width: 100px;
-  max-width: 100%;
-  color: #555;
-  font-size: 16px;
-  background: #fff;
-  border: solid 3px #555;
-  box-sizing: border-box;
-  font-weight: 300;
-  line-height: 22px;
-  font-size: 16px;
-  opacity: 0;
-  animation: ${Fadein} 1s ease forwards 2.2s;
-
-  &:before {
-    content: "";
-    position: absolute;
-    bottom: -24px;
-    left: 50%;
-    margin-left: -15px;
-    border: 12px solid transparent;
-    border-top: 12px solid #fff;
-    z-index: 2;
-  }
-
-  &:after {
-    content: "";
-    position: absolute;
-    bottom: -30px;
-    left: 50%;
-    margin-left: -17px;
-    border: 14px solid transparent;
-    border-top: 14px solid #555;
-    z-index: 1;
-  }
-`;
-
 export default class Thanks extends React.Component {
   constructor(props: any) {
     super(props);
@@ -135,17 +105,19 @@ export default class Thanks extends React.Component {
 
   render() {
     return (
-      <React.Fragment>
+      <Wrapper>
         <Cover />
         <Dialog>
           <Greeting>Thank you!!</Greeting>
           {/* <Telling>I am going to reply you as soon as possible.</Telling> */}
-          <Ballon>I AM GOING TO REPLY YOU AS SOON AS POSSIBLE. Bow Bow!</Ballon>
+          <Ballon time="2.2s" margin="2em 0">
+            I AM GOING TO REPLY YOU AS SOON AS POSSIBLE. BOW BOW!
+          </Ballon>
           <ImgBox>
             <Img src={GrayDogRobot} />
           </ImgBox>
         </Dialog>
-      </React.Fragment>
+      </Wrapper>
     );
   }
 }
