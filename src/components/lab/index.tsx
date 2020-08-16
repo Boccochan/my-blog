9a2ce11cd85e31101cf52135cc8867efe4b4f301
Boccochan <boccochan@gmail.com>
2020-08-16 12:52:24 +0900

    Add burger

---
+++ src/components/lab/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+import { theme } from "@src/styles/color";
+import Burger from "@src/components/lib/Burger";
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  border-top: 1px solid ${theme.colors.graySuperLight};
+`;
+
+export default () => {
+  const handleOpen = () => {
+    console.log("Called");
+  };
+
+  return (
+    <Wrapper>
+      <Burger
+        width={200}
+        color={theme.colors.grayDark}
+        openHandler={handleOpen}
+      />
+      <Burger
+        width={20}
+        color={theme.colors.blueLight}
+        openHandler={handleOpen}
+      />
+      <Burger openHandler={() => console.log("Test desu!!")} />
+    </Wrapper>
+  );
+};
