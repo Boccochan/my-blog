db66061cf3e7e1db31af6ae8a5a037e6d937303c
Boccochan <boccochan@gmail.com>
2020-09-18 00:47:18 +0900

    Add scroll for mobile

--- src/components/lab/study/components/scrum-board/index.tsx
+++ src/components/lab/study/components/scrum-board/index.tsx
@@ -2,11 +2,15 @@ import React, { useState } from "react";
 import Sidebar from "./Sidebar";
 import styled from "styled-components";
 import Board from "./Board";
+import { myMedia } from "@src/styles/custom-media";
 
 const Wrapper = styled.div`
   width: 100%;
   // min-height: 100vh;
   display: flex;
+  ${myMedia.lessThan("ipadpro")`
+    overflow-x: auto;
+  `}
 `;
 
 type Menu = {
